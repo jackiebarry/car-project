86b8837022097e3bfd03e0352f5dd2177f7130a2
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2024-06-06 18:15:22 -0230

    changing API call

--- src/HandleAPI.jsx
+++ src/HandleAPI.jsx
@@ -7,10 +7,8 @@ myHeaders.append(
 const requestOptions = {
   method: "GET",
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    Authorization:
-      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYXJhcGkuYXBwIiwic3ViIjoiNWM3ZTNmMGMtYTYxOC00OGI1LWE5NTYtOTM5OGY5NTY1OGZiIiwiYXVkIjoiNWM3ZTNmMGMtYTYxOC00OGI1LWE5NTYtOTM5OGY5NTY1OGZiIiwiZXhwIjoxNzE2NDY5Nzk5LCJpYXQiOjE3MTU4NjQ5OTksImp0aSI6IjQwYmQ1MTM3LWQyZWEtNGNhZS04OGY4LTkwZmEzNjcwZDc5NCIsInVzZXIiOnsic3Vic2NyaWJlZCI6ZmFsc2UsInN1YnNjcmlwdGlvbiI6bnVsbCwicmF0ZV9saW1pdF90eXBlIjoiaGFyZCIsImFkZG9ucyI6eyJhbnRpcXVlX3ZlaGljbGVzIjpmYWxzZSwiZGF0YV9mZWVkIjpmYWxzZX19fQ.kfDLj7-8WfIXRG9aumibMTpAxtXGWEtb_we_oVWssN4",
+    "x-rapidapi-key": "12989ced08msh2b523603de5ccd7p1c52a5jsn7e33be4174a6",
+    "x-rapidapi-host": "car-api2.p.rapidapi.com",
   },
   redirect: "follow",
 };
@@ -18,7 +16,7 @@ const requestOptions = {
 export async function fetchCarModels(year) {
   try {
     const response = await fetch(
-      `https://carapi.app/api/models?year=${year}&verbose=yes`,
+      `https://car-api2.p.rapidapi.com/api/makes?direction=asc&sort=id&year=${year}&verbose=yes`,
       requestOptions
     );
     const result = await response.text();
