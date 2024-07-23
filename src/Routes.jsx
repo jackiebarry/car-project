03c4cfabdde50d3cd6d69995c8444aa6284a414d
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2024-07-23 14:57:01 -0400

    trying HashRouter

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import Home from "./Home.jsx";
 import SavedCars from "./SavedCars.jsx";
@@ -6,7 +6,7 @@ import FAQs from "./FAQs.jsx";
 import Resources from "./Resources.jsx";
 import CarCompare from "./CarCompare.jsx";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
@@ -37,25 +37,3 @@ const router = createBrowserRouter(
 );
 
 export default router;
-
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-// import Home from "./Home.jsx";
-// import SavedCars from "./SavedCars.jsx";
-// import FAQs from "./FAQs.jsx";
-// import Resources from "./Resources.jsx";
-// import CarCompare from "./CarCompare.jsx";
-
-// const AppRouter = () => (
-//   <HashRouter basename="/car-project">
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/SavedCars" element={<SavedCars />} />
-//       <Route path="/FAQs" element={<FAQs />} />
-//       <Route path="/Resources" element={<Resources />} />
-//       <Route path="/CarCompare" element={<CarCompare />} />
-//     </Routes>
-//   </HashRouter>
-// );
-
-// export default AppRouter;
